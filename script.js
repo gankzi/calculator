b6296bfb599db0fbeb054421b50fef60b7513299
Gankzi <hoonkang1024@gmail.com>
2022-11-04 14:40:32 -0700

    Change calculator logic to solve multiple equations in succession
    Changed calculator logic to seperate out number input and operator input
    to allow the calculator to solve multiple equations in succession.
    Clear display function was also added to reset the calculator
    to it's initial status.

--- script.js
+++ script.js
@@ -1,89 +1,127 @@
 let display = document.querySelector(".display");
 let operation = document.querySelector(".operation");
-let buttons = document.querySelectorAll("button");
+let numberButtons = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
 let equals = document.getElementById("equals");
+let clear = document.getElementById("clear");
 
 let currentOperation = "";
-let currentNumber;
+let defaultNumber = 0;
 let operator;
 let firstNum;
 let secondNum;
+let answer;
 
 function add(num1, num2) {
-    let addNum1 = Number(num1);
-    let addNum2 = Number(num2);
-    return addNum1 + addNum2;
+  let addNum1 = Number(num1);
+  let addNum2 = Number(num2);
+  return addNum1 + addNum2;
 }
 
-function subtract(num1,num2) {
-    return num1 - num2;
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+  return num1 * num2;
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+  return num1 / num2;
 }
 
 function operate(operator, num1, num2) {
-    let answer = operator(num1,num2);
-    display.innerHTML = answer;
+  answer = operator(num1, num2);
+  display.innerHTML = answer;
+  firstNum = undefined;
+  secondNum = undefined;
+  operator = undefined;
 }
 
-buttons.forEach(button => {
-    button.addEventListener("click", function(event) {
-        displayScreen(event.target);
-    })
-})
-
-equals.addEventListener("click", function() {
-    evaluate();
+numberButtons.forEach((numberButton) => {
+  numberButton.addEventListener("click", function (event) {
+    displayNum(event.target);
+  });
 });
 
-function displayScreen(val) {
-    //set Second Number
-    if (operator && val.value !== "=") {
-        if (secondNum) {
-            secondNum += val.value;
-        } else if (!secondNum) {
-            secondNum = val.value;
-        }
-    }
-    
-    //set Operator
-     if (val.name) {
-        if (val.name == "add") {
-            operator = add;
-        } else if (val.name == "subtract") {
-            operator = subtract;
-        } else if (val.name == "divide") {
-            operator = divide;
-        } else {
-            operator = multiply;
-        }
-    }
+operators.forEach((operator) => {
+  operator.addEventListener("click", function (event) {
+    setOperator(event.target);
+  });
+});
 
-    //set First Number
-    if (!operator) {
-        if (firstNum) {
-            firstNum += val.value;
-        } else if (!firstNum) {
-        firstNum = val.value;
-        }
-    }
-    
+equals.addEventListener("click", function (event) {
+  evaluate(event.target);
+});
 
-    console.log(firstNum);
-    console.log(secondNum);    
-
-    currentNumber = val.value;
-    currentOperation = currentOperation + val.value;
-    display.innerHTML = val.value;
-    operation.innerHTML = currentOperation;
+if (clear) {
+  clear.addEventListener("click", function () {
+    clearDisplay();
+  });
 }
 
-function evaluate () {
-    operate(operator,firstNum, secondNum);
+function displayNum(val) {
+  //set First Number
+  if (answer) {
+    firstNum = answer;
+  };
+
+  if (!operator) {
+      if (firstNum && firstNum !== answer) {
+        firstNum += val.value;
+      } else if (!firstNum) {
+        firstNum = val.value;
+      }
+      display.innerHTML = firstNum;
+    }
+  
+
+  //set Second Number
+  if (operator) {
+    if (secondNum) {
+      secondNum += val.value;
+    } else if (!secondNum) {
+      secondNum = val.value;
+    }
+    display.innerHTML = secondNum;
+  }
+
+  currentOperation = currentOperation + val.value;
+  operation.innerHTML = currentOperation;
+}
+
+function setOperator(val) {
+  if (secondNum) {
+    operate(operator, firstNum, secondNum);
+  }
+
+  // //set Operator
+  if (val.name == "add") {
+    operator = add;
+  } else if (val.name == "subtract") {
+    operator = subtract;
+  } else if (val.name == "divide") {
+    operator = divide;
+  } else {
+    operator = multiply;
+  }
+
+  currentOperation = currentOperation + val.value;
+  operation.innerHTML = currentOperation;
+}
+
+function evaluate(val) {
+  currentOperation = currentOperation + val.value;
+  operation.innerHTML = currentOperation;
+  operate(operator, firstNum, secondNum);
+}
+
+function clearDisplay() {
+  firstNum = undefined;
+  secondNum = undefined;
+  operator = undefined;
+  answer = undefined;
+  currentOperation = "";
+  operation.innerHTML = currentOperation;
+  display.innerHTML = defaultNumber;
 }
