3ad7c5a24e1c1aa747d862224aa716d9f4077fdc
Gankzi <hoonkang1024@gmail.com>
2022-11-03 22:06:56 -0700

    Add logic to display numbers when buttons are pressed
    Added display to screen logic to display whatever button
    user presses on to the screen.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let display = document.querySelector(".display");
+let operation = document.querySelector(".operation");
+let buttons = document.querySelectorAll("button");
+
+let currentOperation = "";
+let currentNumber;
+let firstNum;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -16,5 +24,21 @@ function divide(num1, num2) {
 
 function operate(operator, num1, num2) {
     return operator(num1,num2);
+}
+
+buttons.forEach(button => {
+    button.addEventListener("click", function(event) {
+        displayScreen(event.target.value);
+    })
+})
+
+function displayScreen(buttonPressed) {
+    if (currentNumber === undefined) {
+        firstNum = buttonPressed;
+    }
+    currentNumber = buttonPressed;
+    currentOperation = currentOperation + buttonPressed;
+    display.innerHTML = buttonPressed;
+    operation.innerHTML = currentOperation;
 }
 
