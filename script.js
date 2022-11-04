59bb186fef4dc43e731e946997d2be87c1734f58
Gankzi <hoonkang1024@gmail.com>
2022-11-04 00:32:23 -0700

    Update calulator logic to solve equation involving two numbers
    Updated logic to solve any equation involving only two numbers.
    Setting first number, second number and operator is definded through logic.
    Not yet capable of solving equation involving more than two numbers in
    sequence.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let display = document.querySelector(".display");
 let operation = document.querySelector(".operation");
 let buttons = document.querySelectorAll("button");
+let equals = document.getElementById("equals");
 
 let currentOperation = "";
 let currentNumber;
+let operator;
 let firstNum;
+let secondNum;
 
 function add(num1, num2) {
-    return num1 + num2;
+    let addNum1 = Number(num1);
+    let addNum2 = Number(num2);
+    return addNum1 + addNum2;
 }
 
 function subtract(num1,num2) {
@@ -23,22 +28,62 @@ function divide(num1, num2) {
 }
 
 function operate(operator, num1, num2) {
-    return operator(num1,num2);
+    let answer = operator(num1,num2);
+    display.innerHTML = answer;
 }
 
 buttons.forEach(button => {
     button.addEventListener("click", function(event) {
-        displayScreen(event.target.value);
+        displayScreen(event.target);
     })
 })
 
-function displayScreen(buttonPressed) {
-    if (currentNumber === undefined) {
-        firstNum = buttonPressed;
+equals.addEventListener("click", function() {
+    evaluate();
+});
+
+function displayScreen(val) {
+    //set Second Number
+    if (operator && val.value !== "=") {
+        if (secondNum) {
+            secondNum += val.value;
+        } else if (!secondNum) {
+            secondNum = val.value;
+        }
     }
-    currentNumber = buttonPressed;
-    currentOperation = currentOperation + buttonPressed;
-    display.innerHTML = buttonPressed;
+    
+    //set Operator
+     if (val.name) {
+        if (val.name == "add") {
+            operator = add;
+        } else if (val.name == "subtract") {
+            operator = subtract;
+        } else if (val.name == "divide") {
+            operator = divide;
+        } else {
+            operator = multiply;
+        }
+    }
+
+    //set First Number
+    if (!operator) {
+        if (firstNum) {
+            firstNum += val.value;
+        } else if (!firstNum) {
+        firstNum = val.value;
+        }
+    }
+    
+
+    console.log(firstNum);
+    console.log(secondNum);    
+
+    currentNumber = val.value;
+    currentOperation = currentOperation + val.value;
+    display.innerHTML = val.value;
     operation.innerHTML = currentOperation;
 }
 
+function evaluate () {
+    operate(operator,firstNum, secondNum);
+}
